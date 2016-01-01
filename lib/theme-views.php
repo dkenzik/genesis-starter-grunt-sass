47379069a65373a2fed63a9ff6a1e5c28373f139
David Kenzik <david@kenzik.com>
2016-01-01 16:42:48 -0500

    added utility bar; still need to tweak styles; removed responsive-nav (using a plugin)

--- lib/theme-views.php
+++ lib/theme-views.php
@@ -3,3 +3,38 @@
 /**
  * Custom queries, hooks, etc go here
  */
+
+// Setup Utility Bar
+genesis_register_sidebar( array(
+  'id' => 'utility-bar-left',
+  'name' => __( 'Utility Bar Left', 'theme-prefix' ),
+  'description' => __( 'This is the left utility bar above the header.', 'theme-prefix' ),
+  ) 
+);
+
+genesis_register_sidebar( array(
+  'id' => 'utility-bar-right',
+  'name' => __( 'Utility Bar Right', 'theme-prefix' ),
+  'description' => __( 'This is the right utility bar above the header.', 'theme-prefix' ),
+  ) 
+);
+
+add_action( 'genesis_before_header', 'utility_bar' );
+
+function utility_bar() {
+
+  echo '<div class="utility-bar"><div class="wrap">';
+
+  genesis_widget_area( 'utility-bar-left', array(
+    'before' => '<div class="utility-bar-left">',
+    'after' => '</div>',
+  ) );
+ 
+  genesis_widget_area( 'utility-bar-right', array(
+    'before' => '<div class="utility-bar-right">',
+    'after' => '</div>',
+  ) );
+ 
+  echo '</div></div>';
+ 
+}
